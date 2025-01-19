4c7c948a319c54199db5164dec2a67091896adb9
Ashish0243 <ashishrajpal73@gmail.com>
2025-01-19 21:41:59 +0530

    zoom in

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -16,7 +16,7 @@ const MapComponent = ({ location, accuracy, otherUsers }) => {
                 maxZoom: 18,
                 zoomSnap: 0.5,
                 zoomDelta: 1,
-            }).setView([0, 0], 13);
+            }).setView([0, 0], 6);
 
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
